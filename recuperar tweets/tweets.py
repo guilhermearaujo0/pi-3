65e6d5a63af693b829218c6780143acbe0706c98
Guilherme Araujo <81876528+guilhermearaujo0@users.noreply.github.com>
2022-11-28 00:56:55 -0300

    atualização de dashboard e readme

--- recuperar tweets/tweets.py
+++ recuperar tweets/tweets.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 query = "#AFazenda14"
 tweets = []
-limit = 15874
+limit = 38410
 
 for tweet in sntwitter.TwitterSearchScraper(query).get_items():
 
@@ -12,14 +12,14 @@ for tweet in sntwitter.TwitterSearchScraper(query).get_items():
     if len(tweets) == limit:
         break
     else:
-        tweets.append([tweet.id, "2022-10-17 18:16:12", tweet.username, tweet.content,
+        tweets.append([tweet.id, tweet.date, tweet.username, tweet.content,
                       tweet.replyCount, tweet.retweetCount, tweet.likeCount, tweet.quoteCount])
 
 df = pd.DataFrame(tweets, columns=[
                   'ID', 'Date', 'User', 'Tweet', 'ReplyCount', 'RTCount', 'LikeCount', 'QuoteCount'])
 print(df)
 df['ID'] = df['ID'].astype(str)
-# df['Date'] = df['Date'].astype(str).str[:-6]  // tweet.date
+df['Date'] = df['Date'].astype(str).str[:-6]  # //  2022-10-17 18:16:12
 # to save to csv
 df.to_excel('tweets17-10.xlsx')
 # to save to csv
